7de0fb724943fe3dde8faa95508c7fa0fd2f446d
Mzyy <huhuyaho3@gmail.com>
2026-05-23 07:34:04 +0700

    Yeah

--- Plugun_toolbox_V6.lua
+++ Plugun_toolbox_V6.lua
@@ -1,1 +1,9 @@
+print("KONFIRMASI RESMI")
 
+print("Plugin ini resmi kami tutup paksa.")
+
+print("Penutupan dilakukan karena adanya pelanggaran dari pihak pengguna internal yang menyebarkan plugin berbayar secara gratis kepada user lain tanpa izin.")
+
+print("Akibat kejadian tersebut, seluruh layanan, update, dan akses plugin dihentikan mulai hari ini.")
+
+print("Terima kasih kepada semua user yang telah mendukung plugin ini dengan benar.")
